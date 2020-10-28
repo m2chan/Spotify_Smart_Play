83f1f144d7823467c9fde081a581ef60a3b6c98c
Marco Chan <marcochan@Marcos-MacBook-Pro.local>
2020-10-28 19:53:49 -0400

    Simplified code

--- refresh.py
+++ refresh.py
@@ -3,28 +3,25 @@
 from config import refresh_token, base_64_id
 import requests
 
-class Refresh(object):
-    def __init__(self):
-        self.refresh_token = refresh_token
-        self.base_64_id = base_64_id
+def refresh():
+    '''
+    Uses the refresh token to generate a new Spotify API access token
+    '''
+    query = 'https://accounts.spotify.com/api/token'
+    
+    header = {
+        'Authorization': f'Basic {base_64_id}'
+    }
+    
+    data = {
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token
+    }
+    
+    response = requests.post(query, data=data, headers=header)
+    response_json = response.json()
 
-    def refresh(self):
-        query = 'https://accounts.spotify.com/api/token'
-        
-        header = {
-            'Authorization': f'Basic {base_64_id}'
-        }
-        
-        data = {
-            'grant_type': 'refresh_token',
-            'refresh_token': refresh_token
-        }
-        
-        response = requests.post(query, data=data, headers=header)
-        response_json = response.json()
-
-        return response_json['access_token']
+    return response_json['access_token']
 
 if __name__ == '__main__':
-    refresh = Refresh()
-    print(refresh.refresh())
+    print(refresh())
